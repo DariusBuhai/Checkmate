8907be6aa9e542e4e5a02496495be61c41a62d41
Johnny <Johnny.savu.99@gmail.com>
2020-03-26 23:24:59 +0200

    Piese

--- include/piece.h
+++ include/piece.h
@@ -4,10 +4,68 @@
 
 #ifndef CHECKMATE_PIECE_H
 #define CHECKMATE_PIECE_H
+#include <vector>
+#include <string>
 
 class Piece{
-private:
+protected:
     int x_position, y_position;
+    bool has_moved;
+    bool color;
+    std::string name;
+    //0 is for white, 1 is for black;
+protected:
+    static bool isInTable(int, int);
+    Piece(int, int, bool);
+    void set_position(int, int);
+    
 };
+
+class Pawn : public Piece
+{
+
+public:
+    Pawn(int, int, bool);
+    //we are going to return a path of the piece to check later on the board if 
+    //that position is reachable by the path  
+    std::vector<std::pair<int, int>> path(int, int);
+    //first we check if we can get there
+    bool isValidPosition(int, int);
+
+
+};
+/*
+class Rook : public Piece
+{
+public:
+    void move(int, int, int);
+
+};
+
+class Knight : public Piece
+{
+public:
+    void move(int, int, int);
+};
+
+class Bishop : public Piece
+{
+public:
+    void move(int, int, int);
+};
+
+class Queen : public Piece
+{
+public:
+    void move(int, int, int);
+};
+
+class King : public Piece
+{
+public:
+    void move(int, int, int);
+};
+
+*/
 
 #endif //CHECKMATE_PIECE_H
---
+++ src/piece.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include "piece.h"
+
+
+// --------- piece methods ----------
+Piece::Piece(int x_position, int y_position, bool color) : x_position(x_position), 
+y_position(y_position), has_moved(false){}
+
+void Piece::set_position(int x_next, int y_next)
+{
+    x_position = x_next;
+    y_position = y_next;
+}
+
+bool Piece::isInTable(int x, int y)
+{
+    return ((x>= 1 && x<= 8) && (y>= 1 && y <= 8));
+}
+
+//---------- Pawn methods -----------
+
+Pawn::Pawn(int x_position, int y_position, bool color) : Piece(x_position, y_position, color)
+{
+    name = "Pawn";
+}
+
+
+bool Pawn::isValidPosition(int x_next,int y_next)
+{
+    int dist = 1; //maximum distance it can move 
+    if (!Piece::isInTable(x_next, y_next))
+        return 0;
+    if (has_moved == false)
+        dist = 2;
+    if (color == 0) //if it's white, it has to go up
+        {
+            if (x_next == x_position)
+                if (y_next - y_position <= dist && y_next > y_position)
+                    return 1;
+        }
+    if (color == 1) // it has to go down
+        if (x_next == x_position)
+            if (y_position - y_next <= dist && y_position > y_next)
+                return 1;
+    return 0;
+}
+
+std::vector<std::pair<int, int>> Pawn::path(int x_next, int y_next)
+{
+    std::vector< std::pair<int,int >> p;
+    int posx, posy;
+    posx = x_position;
+    posy = y_position;
+    // we go up
+    if (y_next > y_position)
+        while (posy < y_next)
+        {
+            posy ++ ;
+            p.push_back(std::make_pair(posx, posy));
+        }
+    // we go down
+    else
+    {
+        while (posy > y_next)
+        {
+            posy -- ;
+            p.push_back(std::make_pair(posx, posy));
+        }
+    }
+    p.clear();
+    
+}
