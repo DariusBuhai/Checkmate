7b0a1d6e174ad8281dc7767c8639a0303c9d3dfa
Darius Buhai <buhaidarius@gmail.com>
2020-03-26 11:51:44 +0200

    Added sfml

---
+++ SFML/build/src/SFML/System/CMakeFiles/sfml-system.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "../../../lib/libsfml-system.2.5.1.dylib"
+  "../../../lib/libsfml-system.2.5.dylib"
+  "../../../lib/libsfml-system.dylib"
+  "../../../lib/libsfml-system.pdb"
+  "CMakeFiles/sfml-system.dir/Clock.cpp.o"
+  "CMakeFiles/sfml-system.dir/Err.cpp.o"
+  "CMakeFiles/sfml-system.dir/FileInputStream.cpp.o"
+  "CMakeFiles/sfml-system.dir/Lock.cpp.o"
+  "CMakeFiles/sfml-system.dir/MemoryInputStream.cpp.o"
+  "CMakeFiles/sfml-system.dir/Mutex.cpp.o"
+  "CMakeFiles/sfml-system.dir/Sleep.cpp.o"
+  "CMakeFiles/sfml-system.dir/String.cpp.o"
+  "CMakeFiles/sfml-system.dir/Thread.cpp.o"
+  "CMakeFiles/sfml-system.dir/ThreadLocal.cpp.o"
+  "CMakeFiles/sfml-system.dir/Time.cpp.o"
+  "CMakeFiles/sfml-system.dir/Unix/ClockImpl.cpp.o"
+  "CMakeFiles/sfml-system.dir/Unix/MutexImpl.cpp.o"
+  "CMakeFiles/sfml-system.dir/Unix/SleepImpl.cpp.o"
+  "CMakeFiles/sfml-system.dir/Unix/ThreadImpl.cpp.o"
+  "CMakeFiles/sfml-system.dir/Unix/ThreadLocalImpl.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml-system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFApplication.m" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFApplication.m.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFApplicationDelegate.m" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFApplicationDelegate.m.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFSilentResponder.m" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFSilentResponder.m.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFWindow.m" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFWindow.m.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "SFML_WINDOW_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Clipboard.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Clipboard.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Context.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Context.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Cursor.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Cursor.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/GlContext.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/GlContext.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/GlResource.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Joystick.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Joystick.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/JoystickManager.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/JoystickManager.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Keyboard.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Keyboard.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Mouse.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Mouse.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/AutoreleasePoolWrapper.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/AutoreleasePoolWrapper.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/ClipboardImpl.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/ClipboardImpl.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/CursorImpl.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/CursorImpl.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/HIDInputManager.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/HIDInputManager.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/HIDJoystickManager.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/HIDJoystickManager.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/InputImpl.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/InputImpl.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/JoystickImpl.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/JoystickImpl.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/NSImage+raw.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/NSImage+raw.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFContext.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFContext.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFKeyboardModifiersHelper.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFKeyboardModifiersHelper.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFOpenGLView+keyboard.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFOpenGLView+keyboard.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFOpenGLView+mouse.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFOpenGLView+mouse.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFOpenGLView.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFOpenGLView.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFViewController.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFViewController.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SFWindowController.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SFWindowController.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/SensorImpl.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/SensorImpl.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/VideoModeImpl.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/VideoModeImpl.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/WindowImplCocoa.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/WindowImplCocoa.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/cg_sf_conversion.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/cg_sf_conversion.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/OSX/cpp_objc_conversion.mm" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/OSX/cpp_objc_conversion.mm.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Sensor.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/SensorManager.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/SensorManager.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Touch.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Touch.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/VideoMode.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/VideoMode.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/Window.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/Window.cpp.o"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/src/SFML/Window/WindowImpl.cpp" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_WINDOW_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/lib/libsfml-window.2.5.dylib" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/lib/libsfml-window.2.5.1.dylib"
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/lib/libsfml-window.dylib" "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/lib/libsfml-window.2.5.1.dylib"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dariusbuhai/Desktop/Programs/C++/Checkmate/SFML/build/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
