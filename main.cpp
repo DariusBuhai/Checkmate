47327f130350aeb344cc8e0c5e7964b554b91395
Vlad <vlad_ciorica@yahoo.com>
2020-03-27 00:24:32 +0200

    testezsavaddacamerge

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "piece.h"
+#include "draw.h"
 
-
-int main(){
-
+int main()
+{
+    std::cout<<1;
+    Draw d;
+    d.draw();
     Pawn p(1, 1, 0);
     std::cout << p.isValidPosition(1, 4);
-
     return 0;
 }
