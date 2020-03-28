73f15c320ed8d132ba7902984062e0d61aba2d28
Darius Buhai <buhaidarius@gmail.com>
2020-03-28 14:44:23 +0200

    Deleted

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main()
 
 	Draw d;
 	d.draw();
-	King P(1, 1, 0);
+	King P(1, 1, false);
 	/*std::vector<std::vector< std::pair<int, int > > > p;
 	p = P.path();
 	for (auto way : p)
-	{F
+	{
 		for (auto pos : way)
 		{
 			std::cout << pos.first << " " << pos.second << "\n";
