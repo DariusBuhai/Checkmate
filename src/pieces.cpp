1f679512556d9fd1b099914536db0187adc85667
JohnnySavu <johnny.savu.99@gmail.com>
2020-04-09 21:44:30 +0300

    Solved Problem

--- src/pieces.cpp
+++ src/pieces.cpp
@@ -103,8 +103,10 @@ void Pieces::movePiece(Piece* piece, std::pair<int, int> new_position){
     if(piece->getType()=="Pawn" && ((piece->getPlayer()==1 && piece->getPos().second==7) || (piece->getPlayer()==0 && piece->getPos().second==0))){
         for(int i=0;i<this->pieces.size();i++)
             if(pieces[i]==piece){
-                delete pieces[i];
-                pieces[i] = new Queen(piece->getPos(), piece->getPlayer());;
+                pieces.push_back(new Queen(piece->getPos(), piece->getPlayer()));
+                (*(pieces[i])).~Piece();
+                delete (pieces[i]);
+                pieces.erase(pieces.begin() + i);
             }
     }
 
