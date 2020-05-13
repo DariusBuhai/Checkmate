098d9ba7d87ed89590e193080c70b465c3f8af36
Darius Buhai <buhaidarius@gmail.com>
2020-05-13 12:37:42 +0300

    Removed from connector

--- include/connector.h
+++ include/connector.h
@@ -18,7 +18,7 @@
     {
         pipin_w = pipin_r = pipout_w = pipout_r = NULL;
         sats.nLength = sizeof(sats);
-        sats.bInheritHandle = TRUE;
+        sats.bInheritHandle = true;
         sats.lpSecurityDescriptor = NULL;
 
         CreatePipe(&pipout_r, &pipout_w, &sats, 0);
@@ -30,7 +30,7 @@
         sti.hStdOutput = pipout_w;
         sti.hStdError = pipout_w;
 
-        CreateProcess(NULL, path, NULL, NULL, TRUE,0, NULL, NULL, &sti, &pi);
+        CreateProcess(NULL, path, NULL, NULL, true,0, NULL, NULL, &sti, &pi);
     }
 
 
@@ -69,60 +69,6 @@
         if(pi.hThread != NULL) CloseHandle(pi.hThread);
     }
 #else
-    #include <unistd.h>
-    #include <stdio.h>
-    #include <iostream>
-    #include <string>
 
-    /*STARTUPINFO sti = {0};
-    SECURITY_ATTRIBUTES sats = {0};
-    PROCESS_INFORMATION pi = {0};
-    HANDLE pipin_w, pipin_r, pipout_w, pipout_r;
-    BYTE buffer[2048];
-    DWORD writ,available,Read,excode;
-    void ConnectToEngine(char* path){
-        pipin_w = pipin_r = pipout_w = pipout_r = NULL;
-        sats.nLength = sizeof(sats);
-        sats.bInheritHandle = TRUE;
-        sats.lpSecurityDescriptor = NULL;
-        CreatePipe(&pipout_r, &pipout_w, &sats, 0);
-        CreatePipe(&pipin_r, &pipin_w, &sats, 0);
-        sti.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
-        sti.wShowWindow = SW_HIDE;
-        sti.hStdInput = pipin_r;
-        sti.hStdOutput = pipout_w;
-        sti.hStdError = pipout_w;
-        CreateProcess(NULL, path, NULL, NULL, TRUE,0, NULL, NULL, &sti, &pi);
-    }
-    std::string getNextMove(std::string position)
-    {
-        std::string str;
-        position = "position startpos moves "+position+"\ngo\n";
-        WriteFile(pipin_w, position.c_str(), position.length(),&writ, NULL);
-        Sleep(500);
-        PeekNamedPipe(pipout_r, buffer,sizeof(buffer), &Read, &available, NULL);
-        do
-        {
-            ZeroMemory(buffer, sizeof(buffer));
-            if(!ReadFile(pipout_r, buffer, sizeof(buffer), &Read, NULL) || !Read) break;
-            buffer[Read] = 0;
-            str+=(char*)buffer;
-        }
-        while(Read >= sizeof(buffer));
-        int n = str.find("bestmove");
-        std::cout<<str<<'\n';
-        if (n!=-1) return str.substr(n+9,4);
-        return "error";
-    }
-    void CloseConnection()
-    {
-        WriteFile(pipin_w, "quit\n", 5,&writ, NULL);
-        if(pipin_w != NULL) CloseHandle(pipin_w);
-        if(pipin_r != NULL) CloseHandle(pipin_r);
-        if(pipout_w != NULL) CloseHandle(pipout_w);
-        if(pipout_r != NULL) CloseHandle(pipout_r);
-        if(pi.hProcess != NULL) CloseHandle(pi.hProcess);
-        if(pi.hThread != NULL) CloseHandle(pi.hThread);
-    }*/
 #endif
 #endif //CONNECTOR_H
