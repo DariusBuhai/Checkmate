caf89620c00447c3dca5dae6abb716e1eaa93487
Darius Buhai <buhaidarius@gmail.com>
2020-05-19 12:58:35 +0300

    .

--- src/brain.cpp
+++ src/brain.cpp
@@ -144,16 +144,17 @@ bool Brain::isOkToMove(Piece* piece, std::pair<int,int> position)
     board[piece -> getPlayer()][position.first][position.second] = nullPiece;
     board[piece -> getPlayer()][piece -> getPos().first][piece -> getPos().second] = piece;
 
-    /*Evaluation evalProtect = evalProtected(piece,position);
-    Evaluation evalAttack = evalAttacked(piece,position);
-    if(evalAttack.nr_pieces <= evalProtect.nr_pieces && evalAttack.eval <= evalProtect.eval)
-        return true;
-    return false;*/
-    ///init statement
-    if(auto evalProtect = evalProtected(piece,position), evalAttack = evalAttacked(piece,position);evalAttack.nr_pieces <= evalProtect.nr_pieces && evalAttack.eval <= evalProtect.eval)
-        return true;
-    return false;
-
+    #if defined(_WIN32)
+        Evaluation evalProtect = evalProtected(piece,position);
+        Evaluation evalAttack = evalAttacked(piece,position);
+        if(evalAttack.nr_pieces <= evalProtect.nr_pieces && evalAttack.eval <= evalProtect.eval)
+            return true;
+        return false;
+    #else
+        if(auto evalProtect = evalProtected(piece,position), evalAttack = evalAttacked(piece,position);evalAttack.nr_pieces <= evalProtect.nr_pieces && evalAttack.eval <= evalProtect.eval)
+            return true;
+        return false;
+    #endif
 }
 
 bool Brain::canCheck(Piece* piece, std::pair<int,int> position)
